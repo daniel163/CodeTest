53f4ec0d0242f582f96a70cc33a302704ec7a5af
nickzhang <nickzhang@tcl.com>
2017-03-22 16:50:45 +0800

    update

--- src/main/java/com/codetest/test/codetest01.java
+++ src/main/java/com/codetest/test/codetest01.java
@@ -1,10 +1,33 @@
 package com.codetest.test;
 
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
 /**
+ * FileChannel读取数据到Buffer中的示例
  * Created by nickzhang on 2017/3/22.
  */
 public class codetest01 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        RandomAccessFile aFile = new RandomAccessFile("data/nio-data.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
 
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+
+            while(buf.hasRemaining()){
+                System.out.print((char) buf.get());
+            }
+
+            buf.clear();
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
     }
 }

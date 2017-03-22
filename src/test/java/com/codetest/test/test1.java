c8da46f3a1baba20ed0324d9d377a6866faa585c
nickzhang <nickzhang@tcl.com>
2017-03-22 18:32:47 +0800

    update

---
+++ src/test/java/com/codetest/test/test1.java
@@ -0,0 +1,10 @@
+package com.codetest.test;
+
+/**
+ * Created by nickzhang on 2017/3/22.
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.printf("hello ！");
+    }
+}
